1e47427855b02680bd85401103a070b7ca1e029e
narciso castro <narcisocastro0802@gmail.com>
2024-06-13 23:14:28 -0700

    Completed all methods

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace IterationStatements
+﻿using System.ComponentModel.Design;
+using System.Reflection;
+
+namespace IterationStatements
 {
     public class Program
     {
@@ -27,7 +30,128 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
+            MultiplyNumber();
+        }
+
+        public static void PrintThousand()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void EveryThree()
+        {
+            for (int i = 3; i < 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static bool CheckEquality(int a, int b)
+        {
+            if(a == b)
+            {
+                return true;
+            }
+
+            return false;
+                
+        }   
+        
+        public static void NumberCheck()
+        {
+            int i = 3;
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
             
         }
+
+        public static void CheckSign()
+        {
+            int i = -9;
+            if (i > 0)
+            {
+                Console.WriteLine("This is a postive number.");
+            }
+            else
+            {
+                Console.WriteLine("This is a negative number.");
+            }
+        }
+
+        public static void CheckEligibility()
+        {
+            Console.WriteLine("What is your age?");
+            string input = Console.ReadLine();
+
+            int age;
+            bool isParsable = int.TryParse(input, out age);
+
+            if (isParsable)
+            {
+                if (age >= 18)
+                {
+                    Console.WriteLine("You are eligible to vote.");
+                }
+                else
+                {
+                    Console.WriteLine("You are not eligible to vote.");
+                }
+            }
+        }
+
+        public static void CheckRange()
+        {
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+
+            int number;
+            bool isParsable = int.TryParse(input, out number);
+
+            if (isParsable)
+            {
+                int lowerBound = -10;
+                int upperBound = 10;
+
+                if (number >= lowerBound && number <= upperBound)
+                {
+                    Console.WriteLine($"The number {number} is within the range {lowerBound} to {upperBound}.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {number} is not within range {lowerBound} to {upperBound}.");
+                }
+            }
+
+            
+        }
+        public static void MultiplyNumber()
+        {
+            Console.WriteLine("Enter a number to display its multiplication table: ");
+            string input = Console.ReadLine();
+
+            int num;
+            bool isParsable = int.TryParse(input, out num);
+
+            if (isParsable)
+            {
+                for(int i = 1; i <= 12; i++)
+                {
+                    int result = num * i;
+                    Console.WriteLine($"{num} x {i} = {result}");
+                }
+            }
+               
+        }    
+
+
     }
 }
